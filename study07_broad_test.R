ccaf505723bbe870306d761f7d758faa490bffdd
jhhughes256 <jhhughes256@gmail.com>
2017-06-14 15:31:06 +0930

    Begun testing changes in optim.sumexp
    Changes as outlined in #7

---
+++ study07_broad_test.R
@@ -0,0 +1,191 @@
+# Create a function that is able to parse a matrix containing alot of data
+# -----------------------------------------------------------------------------
+# Set up directories
+  if (!exists("git.dir")) {
+    rm(list = ls(all = T))
+    wd <- c("C:/Users/Jim Hughes/Documents", "C:/Users/hugjh001/Documents",
+      "C:/Users/hugjh001/Desktop", "C:/windows/system32")
+
+    graphics.off()
+    if (getwd() == wd[1]) {
+      gir.dir <- paste0(getwd(), "/GitRepos")
+      reponame <- "optinterval"
+    } else if (getwd() == wd[2]) {
+      git.dir <- getwd()
+      reponame <- "optinterval"
+    } else if (getwd() == wd[3] | getwd() == wd[4]) {
+      git.dir <- "E:/Hughes/Git"
+      reponame <- "splines"
+    }
+    rm("wd")
+  }
+
+# Load packages
+  library(GA)
+  #library(ggplot2)
+  #theme_bw2 <- theme_set(theme_bw(base_size = 14))
+  #theme_update(plot.title = element_text(hjust = 0.5))
+
+# Source scripts to set up environment
+  set.seed(256256)
+  niter <- 1000
+  source(paste(git.dir, reponame, "study_functions.R", sep = "/"))
+  source(paste(git.dir, reponame, "study_rdata.R", sep = "/"))
+
+# -----------------------------------------------------------------------------
+# Set basic parameters
+
+  data.names <- paste0("d", as.vector(outer(1:3, c("b", "a"), paste0)))[-1]
+  par.names <- paste(data.names, "p", sep = ".")
+  fn.names <- paste("pred", data.names, sep = ".")
+  t2.names <- paste(data.names, "t", sep = ".")
+
+  study.fn <- function(data, par, fn, nobs, t2, sdev = 0.05, tlast = 24, logauc = F) {
+    niter <- dim(data)[2]
+    absorp <- ifelse((dim(par)[1] %% 2) != 0, T, F)
+    t1 <- seq(0, tlast, length.out = nobs)
+    err <- matrix(
+      1 + rnorm(n = length(t1)*niter, mean = 0, sd = sdev),
+      nrow = length(t1), ncol = niter
+    )
+    subd <- data[which(time.samp %in% t1),]*err
+    fit.par <- apply(subd, 2, function(x) {
+      chisq.sumexp(optim.sumexp(
+        data.frame(time = t1, conc = x), oral = absorp
+      ))$par
+    })
+
+    t2 <- sapply(fit.par, simplify = "array",
+      FUN = function(x) c(0, round(optim.interv(t1, x)$par, 1), tlast)
+    )
+    t3.tmax <- sapply(fit.par, tmax.sumexp)
+    t3 <- mapply(fit.par, t3.tmax,
+      FUN = function(x, y) {
+        z <- c(0, round(optim.interv(t1, x, tmax = y)$par, 1), y, tlast)
+        return(z[order(z)])
+    })
+
+    auc <- data.frame(
+      true = apply(par, 2, function(x) integrate(fn, 0, tlast, p = x)$value),
+      basic = apply(par, 2, function(x) auc.interv(t1, x, fn)),
+      optint = mapply(data.frame(par), data.frame(t2),
+        FUN = function(x, y) auc.interv(y, x, fn)
+      ),
+      optintwCmax = mapply(data.frame(par), data.frame(t3),
+        FUN = function(x, y) auc.interv(y, x, fn)
+      )
+    )
+    cmax <- data.frame(
+      true = apply(par, 2, function(x) pred.sumexp(x, tmax.sumexp(x))),
+      basic = apply(par, 2, function(x) max(pred.sumexp(x, t1))),
+      optint = mapply(data.frame(par), data.frame(t2),
+        FUN = function(x, y) max(pred.sumexp(x, y))
+      ),
+      optintwCmax = mapply(data.frame(par), data.frame(t3),
+        FUN = function(x, y) max(pred.sumexp(x, y))
+      )
+    )
+    tmax <- data.frame(
+      true = apply(par, 2, tmax.sumexp),
+      basic = mapply(data.frame(par), cmax$basic,
+        FUN = function(x, y) t1[which(pred.sumexp(x, t1) == y)][1]
+      ),
+      optint = mapply(data.frame(par), cmax$optint, data.frame(t2),
+        FUN = function(x, y, z) z[which(pred.sumexp(x, z) == y)][1]
+      ),
+      optintwCmax = mapply(data.frame(par), cmax$optintwCmax, data.frame(t3),
+        FUN = function(x, y, z) z[which(pred.sumexp(x, z) == y)][1]
+      )
+    )
+    return(list(auc = auc, cmax = cmax, tmax = tmax))
+  }
+
+# -----------------------------------------------------------------------------
+
+  run.desc <- c(50, 250, 2)
+  #run.desc <- c(100, 250, 2)
+  #run.desc <- c(50, 500, 2)
+  #run.desc <- c(50, 250, 5)
+  #run.desc <- c(50, 250, 6)
+  #run.desc <- c(50, 250, 7)
+  #run.desc <- c(50, 250, 8)
+
+  optim.sumexp <- function(data, oral = F, nexp = 3) {
+    x <- data[which(data[, 2] > 0 & data[, 1] != 0), 1]
+    y <- data[which(data[, 2] > 0 & data[, 1] != 0), 2]
+    opt.par <- list(NULL)
+    opt.val <- list(NULL)
+    opt.gra <- list(NULL)
+    opt.con <- list(NULL)
+    opt.mes <- list(NULL)
+    lm.sub <- which(y == max(y))[1]:length(y)
+    lmres <- unname(lm(log(y[lm.sub]) ~ x[lm.sub])$coefficients)
+    for (i in 1:nexp) {
+      if (i == 1 & !oral) {
+        optres <- list(
+          par = c(lmres[2], lmres[1]),
+          value = mle.sumexp(unname(c(lmres[2], lmres[1])), x, y, 0.01),
+          counts = NULL, convergence = 0, message = NULL
+        )
+      } else {
+        browser()
+        k <- 1
+        all.best <- c(NULL)
+        repeat {
+          j <- 1
+          best <- Inf
+          repeat {
+            gares <- ga("real-valued",
+              mle.sumexp, x = x, y = y, ga = T, sigma = 0.01,
+              min = c(rep(lmres[2]*50, i + oral), rep(lmres[1]-2, i)),
+              max = c(rep(lmres[2]/50, i + oral), rep(lmres[1]+2, i)),
+              selection = gareal_lrSelection,
+              crossover = gareal_spCrossover,
+              mutation = gareal_raMutation,
+              maxiter = run.desc[1],
+              popSize = run.desc[2],
+              monitor = F
+            )
+            optres <- optim(
+              gares@solution[1, ],
+              mle.sumexp,
+              method = "BFGS",
+              x = x, y = y, sigma = 0.01
+            )
+            if (optres$value < best) {
+              best <- optres$value
+              j <- 1
+            } else {
+              j <- j + 1
+              if (j == run.desc[3]) break
+            }
+          }
+          all.best <- c(all.best, best)
+          if (k == 1000) break
+          k <- k + 1
+        }
+
+      }
+      opt.par[[i]] <- optres$par
+      opt.val[[i]] <- optres$value
+      opt.gra[[i]] <- optres$counts
+      opt.con[[i]] <- optres$convergence
+      opt.mes[[i]] <- ifelse(is.null(optres$message),
+        "NULL", optres$message)
+    }
+
+    res <- list(par = opt.par, value = opt.val, counts = opt.gra,
+      convergence = opt.con, message = opt.mes)
+    res
+  }
+
+  fin.res <- list(NULL)
+  for (i in 3) {
+    fin.res[[i]] <- list(
+      data = data.names[i],
+      result = study.fn(get(data.names[i]),
+        par = get(par.names[i]), fn = get(fn.names[i]),
+        t2 = get(t2.names[i]), nobs = 9
+      )
+    )
+  }
---
+++ study08_broad_test_analysis.R
@@ -0,0 +1,146 @@
+# Create a function that is able to parse a matrix containing alot of data
+# -----------------------------------------------------------------------------
+# Set up directories
+  if (!exists("git.dir")) {
+    rm(list = ls(all = T))
+    wd <- c("C:/Users/Jim Hughes/Documents", "C:/Users/hugjh001/Documents",
+      "C:/Users/hugjh001/Desktop", "C:/windows/system32")
+
+    graphics.off()
+    if (getwd() == wd[1]) {
+      gir.dir <- paste0(getwd(), "/GitRepos")
+      reponame <- "optinterval"
+    } else if (getwd() == wd[2]) {
+      git.dir <- getwd()
+      reponame <- "optinterval"
+    } else if (getwd() == wd[3] | getwd() == wd[4]) {
+      git.dir <- "E:/Hughes/Git"
+      reponame <- "splines"
+    }
+    rm("wd")
+  }
+
+# Load packages
+  #library(GA)
+  library(ggplot2)
+  theme_bw2 <- theme_set(theme_bw(base_size = 14))
+  theme_update(plot.title = element_text(hjust = 0.5))
+  library(plyr)
+  library(grid)
+
+# Source scripts and r objects to set up environment
+  source(paste(git.dir, reponame, "study_functions.R", sep = "/"))
+  source(paste(git.dir, reponame, "sumstat_functions.R", sep = "/"))
+
+# Source data
+  d1a1 <- readRDS(paste(git.dir, reponame, "d1a-broad-50-250.rds", sep = "/"))
+  d1a2 <- readRDS(paste(git.dir, reponame, "d1a-broad-50-500.rds", sep = "/"))
+  d1a3 <- readRDS(paste(git.dir, reponame, "d1a-broad-100-250.rds", sep = "/"))
+
+# -----------------------------------------------------------------------------
+# Data structure
+# d2b[[1]], d3b[[2]], d1a[[3]], d2a[[4]], d3a[[5]]
+# names(d[[#]]) -> "data" "result"
+# names(d[[#]]$result) -> "auc" "cmax" "tmax"
+
+  data.names <- c("d1a1", "d1a2", "d1a3")
+  iter.vals <- c(50, 50, 100)
+  popn.vals <- c(250, 500, 250)
+  slot.names <- c("auc", "cmax", "tmax")
+
+  res <- data.frame(NULL)
+  plotdata <- data.frame(NULL)
+  for (i in 1:3) {
+    r.out <- data.frame(NULL)
+    d.out <- data.frame(NULL)
+    for (j in 1:3) {
+      d.in <- get(data.names[i])[[slot.names[j]]]
+      d.melt <- data.frame(
+        data = data.names[i],
+        iter = iter.vals[i],
+        popn = popn.vals[i],
+        metric = slot.names[j],
+        ref = rep(d.in$true, 3),
+        test = c(d.in$basic,
+          d.in$optint,
+          d.in$optintwCmax),
+        type = c(rep("bas", 1000), rep("opt", 1000), rep("optc", 1000)))
+      d.melt$prop <- with(d.melt, test/ref)
+      d.out <- rbind(d.out, d.melt)
+      r.out <- rbind(r.out,
+        ddply(d.melt, .(type), function(x) {
+          c(data = data.names[i], iter = iter.vals[i], popn = popn.vals[i],
+            metric = slot.names[j], sumfuncBOX(x$prop)
+          )
+        })
+      )
+    }
+    res <- rbind(res, r.out)
+    plotdata <- rbind(plotdata, d.out)
+  }
+
+# -----------------------------------------------------------------------------
+
+  box.plot.fn <- function(metric, data, x, zoom, layout = NULL) {
+    subplot <- plotdata[plotdata$metric == metric & plotdata$data == data, ]
+    subplot$type <- as.factor(subplot$type)
+    levels(subplot$type) <- c("Basic", "Optint", "Optint w/ Cmax")
+
+    plotobj <- ggplot(subplot, aes(x = type, y = prop))
+    plotobj <- plotobj + geom_hline(yintercept = 1, color = "green4", linetype = "dashed")
+    plotobj <- plotobj + geom_boxplot()
+    plotobj <- plotobj + ggtitle(paste("Metric:", metric))
+    plotobj <- plotobj + xlab("\nMethod")
+    plotobj <- plotobj + ylab("Method/Reference Ratio\n")
+    if (zoom) {
+      ylim.box <- boxplot.stats(subplot$prop)$stats[c(1, 5)]
+      plotobj <- plotobj + coord_cartesian(ylim = ylim.box)
+    }
+    if (x) plotobj <- plotobj + stat_summary(fun.y = mean, geom = "point", shape = 4, size = 4)
+    if (is.null(layout)) print(plotobj)
+    else print(plotobj, vp = layout)
+  }
+
+  forest.plot.fn <- function(metric, data, layout = NULL) {
+    subplot <- res[res$metric == metric & res$data == data, ]
+    subplot$type <- factor(subplot$type, levels = rev(subplot$type))
+    levels(subplot$type) <- c("Optint w/ Cmax", "Optint", "Basic")
+    subplot$median <- as.numeric(subplot$median)
+    subplot$q025 <- as.numeric(subplot$q025)
+    subplot$q25 <- as.numeric(subplot$q25)
+    subplot$q75 <- as.numeric(subplot$q75)
+    subplot$q975 <- as.numeric(subplot$q975)
+
+    plotobj <- ggplot(subplot, aes(x = type, y = median))
+    plotobj <- plotobj + geom_linerange(aes(ymin = q025, ymax = q975), color = "red")
+    plotobj <- plotobj + geom_pointrange(aes(ymin = q25, ymax = q75))
+    plotobj <- plotobj + geom_hline(yintercept = 1, lty = 2, color = "green4")
+    plotobj <- plotobj + ggtitle(paste("Metric:", metric))
+    plotobj <- plotobj + xlab("\nMethod")
+    plotobj <- plotobj + scale_y_continuous("Method/Reference Ratio\n")
+    plotobj <- plotobj + coord_flip()
+    if (is.null(layout)) print(plotobj)
+    else print(plotobj, vp = layout)
+  }
+
+# -----------------------------------------------------------------------------
+
+  vplayout <- function(x, y) viewport(layout.pos.row = x, layout.pos.col = y)
+  grid.newpage()
+  pushViewport(viewport(layout = grid.layout(1,3)))
+
+  box.plot.fn("tmax", "d1a1", F, F, vplayout(1,1))
+  box.plot.fn("tmax", "d1a2", F, F, vplayout(1,2))
+  box.plot.fn("tmax", "d1a3", F, F, vplayout(1,3))
+
+  dev.off()
+
+  vplayout <- function(x, y) viewport(layout.pos.row = x, layout.pos.col = y)
+  grid.newpage()
+  pushViewport(viewport(layout = grid.layout(3, 1)))
+
+  forest.plot.fn("tmax", "d1a1", vplayout(1,1))
+  forest.plot.fn("tmax", "d1a2", vplayout(2,1))
+  forest.plot.fn("tmax", "d1a3", vplayout(3,1))
+
+  dev.off()
