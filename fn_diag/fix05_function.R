98b0fefd4edbee0d72d9600a33cec2bd4d7260ed
jhhughes256 <jhhughes256@gmail.com>
2017-06-20 12:43:43 +0930

    Setup complete for broad and narrow runs using new functions
    Once run, these can be analysed using study_analysis scripts.
    Analysis should provide answers to whether the new functions have improved precision and if there are new issues to deal with #7

---
+++ fn_diag/fix05_function.R
@@ -0,0 +1,92 @@
+# Function containing the framework setup in the intial study design
+# -----------------------------------------------------------------------------
+# Set up directories
+  if (!exists("git.dir")) {
+    rm(list = ls(all = T))
+    wd <- c("C:/Users/Jim Hughes/Documents", "C:/Users/hugjh001/Documents",
+      "C:/Users/hugjh001/Desktop", "C:/windows/system32")
+
+    graphics.off()
+    if (getwd() == wd[1]) {
+      gir.dir <- paste0(getwd(), "/GitRepos")
+      reponame <- "optinterval"
+    } else if (getwd() == wd[2]) {
+      git.dir <- getwd()
+      reponame <- "optinterval"
+    } else if (getwd() == wd[3] | getwd() == wd[4]) {
+      git.dir <- "E:/Hughes/Git"
+      reponame <- "splines"
+    }
+    rm("wd")
+  }
+
+# Load packages
+  library(GA)
+  #library(ggplot2)
+  #theme_bw2 <- theme_set(theme_bw(base_size = 14))
+  #theme_update(plot.title = element_text(hjust = 0.5))
+
+# Source scripts to set up environment
+  source(paste(git.dir, reponame, "fn_diag/fix_functions.R", sep = "/"))
+  source(paste(git.dir, reponame, "study_data.R", sep = "/"))
+
+# -----------------------------------------------------------------------------
+# Set basic parameters
+  data.names <- paste0("d", as.vector(outer(1:3, c("a", "b"), paste0)))
+  par.names <- paste(data.names, "p", sep = ".")
+  fn.names <- paste("pred", data.names, sep = ".")
+  t2.names <- paste(data.names, "t", sep = ".")
+
+  study.fn <- function(data, par, fn, nobs, t2, sdev = 0.05, tlast = 24, logauc = F) {
+    absorp <- ifelse((length(par) %% 2) != 0, T, F)
+    t1 <- round(seq(0, tlast, length.out = nobs), 1)
+    e1 <- 1 + rnorm(n = length(t1), mean = 0, sd = sdev)
+    subd <- data.frame(
+      time = t1,
+      conc = with(data, conc[time %in% t1])*e1
+    )
+    fit.par <- chisq.sumexp(optim.sumexp(subd, oral = absorp))$par
+    int.t3 <- optim.interv.rep(fit.par, 2)
+    t3 <- c(0, round(int.t3$par, 1), tlast)
+
+    t4.tmax <- tmax.sumexp(fit.par)
+    int.t4 <- optim.interv.rep(fit.par, 2, tmax = t4.tmax)
+    nr.t4 <- c(0, round(int.t4$par, 1), tmax.sumexp(fit.par), tlast)
+    t4 <- nr.t4[order(nr.t4)]
+
+    fit.fn <- paste0("pred.d", floor(length(fit.par)/2), ifelse(absorp, "a", "b"))
+    auc <- c(
+      true = integrate(fn, 0, tlast, p = par)$value,
+      sumexp = integrate(get(fit.fn), 0, tlast, p = fit.par)$value,
+      basic = auc.interv(t1, par, fn, log = logauc),
+      user = auc.interv(t2, par, fn, log = logauc),
+      optint = auc.interv(t3, par, fn, log = logauc),
+      optintwCmax = auc.interv(t4, par, fn, log = logauc)
+    )
+    cmax <- c(
+      true = pred.sumexp(par, tmax.sumexp(par)),
+      basic = max(pred.sumexp(par, t1)),
+      user = max(pred.sumexp(par, t2)),
+      optint = max(pred.sumexp(par, t3)),
+      optintwCmax = max(pred.sumexp(par, t4))
+    )
+    tmax <- c(
+      true = tmax.sumexp(par),
+      basic = t1[which(pred.sumexp(par, t1) == cmax["basic"])],
+      user = t2[which(pred.sumexp(par, t2) == cmax["user"])],
+      optint = t3[which(pred.sumexp(par, t3) == cmax["optint"])],
+      optintwCmax = t4[which(pred.sumexp(par, t4) == cmax["optintwCmax"])]
+    )
+    return(list(auc = auc, cmax = cmax, tmax = tmax))
+  }
+
+  fin.res <- list(NULL)
+  for (i in 1:6) {
+    fin.res[[i]] <- list(
+      data = data.names[i],
+      result = study.fn(get(data.names[i]),
+        par = get(par.names[i]), fn = get(fn.names[i]),
+        t2 = get(t2.names[i]), nobs = 9
+      )
+    )
+  }
---
+++ fn_diag/fix07_narrow.R
@@ -0,0 +1,88 @@
+# Function containing the framework setup in the intial study design
+# -----------------------------------------------------------------------------
+# Set up directories
+  if (!exists("git.dir")) {
+    rm(list = ls(all = T))
+    wd <- c("C:/Users/Jim Hughes/Documents", "C:/Users/hugjh001/Documents",
+      "C:/Users/hugjh001/Desktop", "C:/windows/system32")
+
+    graphics.off()
+    if (getwd() == wd[1]) {
+      gir.dir <- paste0(getwd(), "/GitRepos")
+      reponame <- "optinterval"
+    } else if (getwd() == wd[2]) {
+      git.dir <- getwd()
+      reponame <- "optinterval"
+    } else if (getwd() == wd[3] | getwd() == wd[4]) {
+      git.dir <- "E:/Hughes/Git"
+      reponame <- "splines"
+    }
+    rm("wd")
+  }
+
+# Load packages
+  library(GA)
+  #library(ggplot2)
+  #theme_bw2 <- theme_set(theme_bw(base_size = 14))
+  #theme_update(plot.title = element_text(hjust = 0.5))
+
+# Source scripts to set up environment
+  source(paste(git.dir, reponame, "study_functions.R", sep = "/"))
+
+# -----------------------------------------------------------------------------
+# Set basic parameters
+  niter <- 1000
+  time.samp <- seq(0, 24, by = 0.05)
+  sdev <- 0.05
+  tlast <- 24
+  nobs <- 9
+  t1 <- c(0, 0.5, 1, 2, 3, 4, 5, 6, 8, 12, 16, 24)
+
+  pred.d1a <- function(x, p) {
+    exp(p[1]*x + p[3]) - exp(p[2]*x + p[3])
+  }
+  d1a.p <- c(-0.2, -0.4, 4)
+  d1a <- pred.d1a(time.samp, d1a.p)
+  times <- seq(0, tlast, length.out = nobs)
+  err <- matrix(
+    1 + rnorm(n = length(times)*niter, mean = 0, sd = sdev),
+    nrow = length(times), ncol = niter
+  )
+  subd <- d1a[which(time.samp %in% times)]*err
+  fit.par <- apply(subd, 2, function(x) {
+    chisq.sumexp(optim.sumexp(
+      data.frame(time = times, conc = x), oral = T
+    ))$par
+  })
+  t2 <- sapply(fit.par, simplify = "array",
+    FUN = function(x) c(0, round(optim.interv.rep(x, 2)$par, 1), tlast)
+  )
+  t3.tmax <- sapply(fit.par, tmax.sumexp)
+  t3 <- mapply(fit.par, t3.tmax,
+    FUN = function(x, y) {
+      z <- c(0, round(optim.interv.rep(x, 2, tmax = y)$par, 1), y, tlast)
+      return(z[order(z)])
+  })
+
+  auc <- data.frame(
+    true = integrate(pred.d1a, 0, tlast, p = d1a.p)$value,
+    user = auc.interv(t1, d1a.p, pred.d1a),
+    optint = apply(t2, 2, function(x) auc.interv(x, d1a.p, pred.d1a)),
+    optintwCmax = apply(t3, 2, function(x) auc.interv(x, d1a.p, pred.d1a))
+  )
+  cmax <- data.frame(
+    true = pred.sumexp(d1a.p, tmax.sumexp(d1a.p)),
+    user = max(pred.sumexp(d1a.p, t1)),
+    optint = apply(t2, 2, function(x) max(pred.sumexp(d1a.p, x))),
+    optintwCmax = apply(t3, 2, function(x) max(pred.sumexp(d1a.p, x)))
+  )
+  tmax <- data.frame(
+    true = tmax.sumexp(d1a.p),
+    user = t1[which(pred.sumexp(d1a.p, t1) == cmax$user[1])],
+    optint = mapply(cmax$optint, data.frame(t2),
+      FUN = function(y, z) z[which(pred.sumexp(d1a.p, z) == y)][1]
+    ),
+    optintwCmax = mapply(cmax$optintwCmax, data.frame(t3),
+      FUN = function(y, z) z[which(pred.sumexp(d1a.p, z) == y)][1]
+    )
+  )
