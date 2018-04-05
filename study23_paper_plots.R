430ab1a36e20e00e541120325a00c70aaa4a5846
jhhughes256 <jhhughes256@gmail.com>
2018-04-05 10:56:32 +0930

    Final changes to analysis scripts and plot scripts for paper

--- study23_paper_plots.R
+++ study23_paper_plots.R
@@ -31,7 +31,7 @@
   source(paste(git.dir, reponame, "sumstat_functions.R", sep = "/"))
 
 # Source files of interest
-  run.string <- "sigfix-AR3024"
+  run.string <- "broadnewdt2-AR3024"
   d2b <- readRDS(paste(git.dir, reponame,
     paste0("fn_diag/d2b-", run.string, ".rds"), sep = "/"))
   d3b <- readRDS(paste(git.dir, reponame,
@@ -528,7 +528,8 @@
         lower <- q25 - (q75 - q25)
         upper <- q75 + (q75 - q25)
         x$outlier <- ifelse(x$prop < lower | x$prop > upper, T, F)
-        x$bioq <- ifelse(x$prop < 0.8 | x$prop > 1.25, F, T)
+        x$bioq <- ifelse(x$prop < 0.9 | x$prop > 1.1, T, F)
+        x$bioq[x$metric == "tmax"] <- ifelse(x$prop[x$metric == "tmax"] < 0.8 | x$prop[x$metric == "tmax"] > 1.2, T, F)
         x
       })
       d.out <- rbind(d.out, d.mid)
@@ -550,9 +551,9 @@
   bioqloline <- data.frame(ref = lineofid, test = lineofid*0.8)
   bioqhiline2 <- data.frame(ref = lineofid, test = lineofid*1.1)
   bioqloline2 <- data.frame(ref = lineofid, test = lineofid*0.9)
-  plotdata2 <- plotdata[plotdata$type %in% c("030", "bas"),]
+  plotdata2 <- plotdata[plotdata$type %in% c("000", "030"),]
   plotdata2$typef <- factor(plotdata2$type)
-  levels(plotdata2$typef) <- c("Algorithm", "Empirical")
+  levels(plotdata2$typef) <- c("Base", "Adjunct")
   statdata <- ddply(plotdata2, .(data, metric, typef), function(x) {
     data.frame(
       percbioq = paste0("bioq = ", signif(sum(x$bioq, na.rm = T)/length(x$bioq)*100, 3), "%"),
@@ -560,16 +561,12 @@
     )
   })
 
-  truevpred.plot <- function(data, metric, n.col) {
+  truevpred.plot <- function(data, metric, n.col, scale) {
     plotsub <- plotdata2[plotdata2$data == data & plotdata2$metric == metric, ]
     statsub <- statdata[statdata$data == data & statdata$metric == metric, ]
     allval <- c(plotsub$ref)
     minval <- min(allval[allval != 0], na.rm = T)
     maxval <- max(allval[allval != Inf], na.rm = T)
-    logscaleticks <- 10^c(
-      seq(from = floor(log10(signif(minval, 1))), to = ceiling(log10(signif(maxval, 1))), by = 1)
-    )
-    logscaleticks2 <- sort(c(logscaleticks, logscaleticks*3))
     valrange <- log10(maxval) - log10(minval)
     p0 <- NULL
     p0 <- ggplot()
@@ -579,58 +576,73 @@
         colour = "green4", linetype = "dashed")
       p0 <- p0 + geom_line(aes(x = ref, y = test), data = bioqloline,
         colour = "green4", linetype = "dashed")
-      p0 <- p0 + scale_x_log10("True tmax", lim = c(minval, maxval), breaks = logscaleticks2)
-      p0 <- p0 + scale_y_log10("Predicted tmax", lim = c(minval, maxval), breaks = logscaleticks2)
+      p0 <- p0 + scale_x_log10("True tmax", lim = c(minval, maxval), breaks = scale)
+      p0 <- p0 + scale_y_log10("Predicted tmax", lim = c(minval, maxval), breaks = scale)
     } else {
       p0 <- p0 + geom_line(aes(x = ref, y = test), data = bioqhiline2,
         colour = "green4", linetype = "dashed")
       p0 <- p0 + geom_line(aes(x = ref, y = test), data = bioqloline2,
         colour = "green4", linetype = "dashed")
       if (metric == "cmax") {
-        p0 <- p0 + scale_x_log10("True Cmax", lim = c(minval, maxval), breaks = logscaleticks)
-        p0 <- p0 + scale_y_log10("Predicted Cmax", lim = c(minval, maxval), breaks = logscaleticks)
+        p0 <- p0 + scale_x_log10("True Cmax", lim = c(minval, maxval), breaks = scale)
+        p0 <- p0 + scale_y_log10("Predicted Cmax", lim = c(minval, maxval), breaks = scale)
       } else {
-        p0 <- p0 + scale_x_log10("True AUC", lim = c(minval, maxval), breaks = logscaleticks)
-        p0 <- p0 + scale_y_log10("Predicted AUC", lim = c(minval, maxval), breaks = logscaleticks)
+        p0 <- p0 + scale_x_log10("True AUC", lim = c(minval, maxval), breaks = scale, labels = scales::comma)
+        p0 <- p0 + scale_y_log10("Predicted AUC", lim = c(minval, maxval), breaks = scale, labels = scales::comma)
       }
     }
     p0 <- p0 + geom_abline(intercept = 0, slope = 1)
-    p0 <- p0 + scale_colour_manual(values = c("red", "blue"))
+    p0 <- p0 + scale_colour_manual(values = c("blue", "red"))
     p0 <- p0 + theme(legend.position = "none")
     p0 <- p0 + facet_wrap(~typef, ncol = n.col)
     p0
   }
 
-  p1 <- truevpred.plot("d3a", "aucinf", 2)
-  p2 <- truevpred.plot("d3a", "cmax", 2)
-  p3 <- truevpred.plot("d3a", "tmax", 2)
-
-  p123 <- plot_grid(p1, p2, p3, nrow = 3,
-    labels = c("A", "B", "C"), label_fontface = "plain"
-  )
+  # truevpred.plot("d3a", "aucinf", 2)
+  # ggsave("truevpred_paganz.png", width = 23.2, height = 11.2, units = "cm")
+  #
+  # theme_update(axis.text.x = element_text(angle = 335))
+  #
+  # p1 <- truevpred.plot("d2b", "aucinf", 2, c(10, 100, 1000, 10000))
+  # p2 <- truevpred.plot("d3b", "aucinf", 2, c(10, 1000, 100000))
+  # p3 <- truevpred.plot("d3a", "aucinf", 2, c(3, 300, 30000))
+  # p4 <- truevpred.plot("d3a", "cmax", 2, c(1, 10, 100))
+  # p5 <- truevpred.plot("d3a", "tmax", 2, c(1, 3, 10))
+  #
+  # plot_grid(p3, p4, p5, p2, ncol = 3, label_fontface = "plain")
+  # ggsave("truevpred_paganz_cow_adj.png", width = 24.2, height = 12.2, units = "cm")
+  #
+  # theme_bw2 <- theme_set(theme_bw(base_size = 14))
+  # theme_update(plot.title = element_text(hjust = 0.5))
 
   plotdata2$dataf <- factor(plotdata2$data)
-  levels(plotdata2$dataf) <- c("2-comp bolus", "3-comp bolus", "1-comp w/ abs", "2-comp w/ abs", "3-comp w/ abs")
+  levels(plotdata2$dataf) <- c("2 compartment bolus", "3 compartment bolus", "1 compartment oral", "2 compartment oral", "3 compartment oral")
   subdata <- ddply(plotdata2, .(data, dataf, typef, metric), function(x) {
     data.frame(
       prop = x$prop,
       CI90lo = quantile(x$prop, probs = 0.05, na.rm = T, names = F),
-      CI90hi = quantile(x$prop, probs = 0.95, na.rm = T, names = F),
-      CI95lo = quantile(x$prop, probs = 0.025, na.rm = T, names = F),
-      CI95hi = quantile(x$prop, probs = 0.975, na.rm = T, names = F)
+      CI75lo = quantile(x$prop, probs = 0.25, na.rm = T, names = F),
+      CI50 = median(x$prop),
+      CI75hi = quantile(x$prop, probs = 0.75, na.rm = T, names = F),
+      CI90hi = quantile(x$prop, probs = 0.95, na.rm = T, names = F)
     )
   })
   plotaucinf <- subdata[subdata$metric == "aucinf", ]
   plotcmax <- subdata[subdata$metric == "cmax" & subdata$data %in% c("d1a", "d2a", "d3a"), ]
   plottmax <- subdata[subdata$metric == "tmax" & subdata$data %in% c("d1a", "d2a", "d3a"), ]
 
-  p4 <- NULL
-  p4 <- ggplot(plotaucinf, aes(x = typef, y = prop))
-  p4 <- p4 + geom_hline(yintercept = c(0.9, 1.1), color = "green4", linetype = "dashed")
-  p4 <- p4 + geom_hline(yintercept = 1, color = "red", linetype = "dashed")
-  # p4 <- p4 + geom_boxplot(aes(ymin = CI90lo, ymax = CI90hi))
-  p4 <- p4 + geom_boxplot(aes(ymin = CI95lo, ymax = CI95hi))
-  p4 <- p4 + xlab("\nMethod")
-  p4 <- p4 + scale_y_continuous("Method/Reference AUC Ratio\n", lim = c(0.1, 1.6))
-  p4 <- p4 + facet_wrap(~dataf, nrow = 1)
-  p4
+  p7 <- NULL
+  p7 <- ggplot(plotaucinf, aes(x = typef, y = prop))
+  p7 <- p7 + geom_hline(yintercept = c(0.9, 1.1), color = "green4", linetype = "dashed")
+  p7 <- p7 + geom_hline(yintercept = 1, color = "red", linetype = "dashed")
+  # p7 <- p7 + geom_boxplot(aes(ymin = CI90lo, ymax = CI90hi))
+  p7 <- p7 + geom_boxplot(stat = "identity",
+    aes(ymin = CI90lo, lower = CI75lo, middle = CI50, upper = CI75hi, ymax = CI90hi)
+  )
+  p7 <- p7 + xlab("\nOptimisation Method")
+  p7 <- p7 + scale_y_continuous("Relative AUC\n", breaks = c(0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2))
+  p7 <- p7 + facet_wrap(~dataf, nrow = 1)
+  p7 + coord_cartesian(ylim = c(0.55, 1.15))
+
+
+  ggsave("Figure 1.png", width = 23.2, height = 11.2, units = "cm")
